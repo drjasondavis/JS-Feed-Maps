5c081d94fb00c1af79d94cef733bb7ab48e92d74
Jason Davis <jdavis@Jasons-MacBook-Air.local>
2012-12-26 22:38:28 -0600

    whitespace trim; better geo formatting

--- map.js
+++ map.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-    
+
     function createMap(lat, long) {
-        var mapOptions = {
+	var mapOptions = {
 	    center: new google.maps.LatLng(lat, long),
-            zoom: 10,
-	    scrollwheel: false, 
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById("map_canvas"),
+	    zoom: 10,
+	    scrollwheel: false,
+	    mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(document.getElementById("map_canvas"),
 				      mapOptions);
 	return map;
     };
@@ -29,7 +29,36 @@ $(document).ready(function() {
 	    return;
 	}
 	latLongPlaceMap[geoPlaceRequestName] = geocoderResult[0].geometry.location;
-	var formattedAddress = geocoderResult[0].formatted_address;
+
+	function formatGeocoderResultName(r) {
+	    var adminLabel = "";
+	    var countryLabel = "";
+	    var localityLabel = "";
+	    for (var key in r) {
+		if (r.hasOwnProperty(key)) {
+		    if (r[key].types[0] === "administrative_area_level_2") {
+			adminLabel = r[key].long_name;
+		    }
+		    if (r[key].types[0] === "country") {
+			countryLabel = r[key].long_name;
+		    }
+		    if (r[key].types[0] === "locality") {
+			localityLabel = r[key].long_name;
+		    }
+		}
+	    }
+	    var fullLabel;
+	    if (localityLabel != "") {
+		fullLabel = localityLabel + ", " + countryLabel;
+	    } else if (adminLabel != "") {
+		fullLabel = adminLabel + ", " + countryLabel;
+	    } else {
+		fullLabel = countryLabel;
+	    }
+	    return fullLabel;
+	};
+
+	var formattedAddress = formatGeocoderResultName(geocoderResult[0].address_components);
 	canonicalPlaceNames[geoPlaceRequestName] = formattedAddress;
 	if (nullMapCount(latLongPlaceMap) == 0) {
 	    callback(geoPlaces, map);
@@ -43,7 +72,7 @@ $(document).ready(function() {
 	if (typeof title === "undefined") {
 	    title = gp.place;
 	}
-	return "<div class='maps-info-window'><a target='_blank' href=" + gp.url + ">" + title + "</a><br/><i>" 
+	return "<div class='maps-info-window'><a target='_blank' href=" + gp.url + ">" + title + "</a><br/><i>"
 	    + dateFormat(postDate, "dddd, mmmm dS, yyyy") + "<br/>" + canonPlace + "</i></div>";
     };
 
@@ -73,19 +102,19 @@ $(document).ready(function() {
 	$('#map_canvas').mouseover(function(){
 	    onMouseOrTouch();
 	});
-	var polyline = new google.maps.Polyline({path: path, map: map, geodesic: true, strokeColor: 'grey'});	
+	var polyline = new google.maps.Polyline({path: path, map: map, geodesic: true, strokeColor: 'grey'});
     };
 
     function animateMarkers(infoWindows, markers) {
 	$.each(markers, function(i, markerOptions) {
 	    var markerTimeout = markers.length * 200 - (i * 200);
 	    setTimeout(function() {
-		var marker = new google.maps.Marker(markerOptions);		
+		var marker = new google.maps.Marker(markerOptions);
 		google.maps.event.addListener(marker, 'click', function () {
 		    $.each(infoWindows, function(i, w) {
 			w.close();
 		    });
-                    infoWindows[i].open(map, marker);
+		    infoWindows[i].open(map, marker);
 		});
 	    }, markerTimeout);
 	});
@@ -104,14 +133,14 @@ $(document).ready(function() {
     Array.prototype.max = function() {
 	return Math.max.apply({}, this);
     };
-    
+
     Array.prototype.min = function() {
 	return Math.min.apply({}, this);
     };
-	
+
 
     function centerMap(geoPlaces, map) {
-	var splitLong = -20; 
+	var splitLong = -20;
 	var overRunPerc = 0.0;
 	function mapLong(l) {
 	    if (l < 0) {
@@ -151,7 +180,7 @@ $(document).ready(function() {
 	maxLat += latBorder;
 	var sw = new google.maps.LatLng(minLat, minLong);
 	var ne = new google.maps.LatLng(maxLat, maxLong);
-	
+
 	varLatLongBounds = new google.maps.LatLngBounds(sw, ne);
 
 	map.fitBounds(varLatLongBounds);
@@ -163,7 +192,7 @@ $(document).ready(function() {
 	$.each(tumblr_api_read.posts, function(i, p) {
 	    var tags = p.tags;
 	    var placeTags = $.map(tags, function(t) {
-		m = placeRegex.exec(t); 
+		m = placeRegex.exec(t);
 		if (m && m.length == 2) {
 		    return m[1];
 		} else {
@@ -176,17 +205,17 @@ $(document).ready(function() {
 	    gp = {
 		place: placeTags[0],
 		time: p['unix-timestamp'],
-		url: p.url,		
+		url: p.url,
 		title: p['regular-title']
 	    };
 	    geoPlaces.push(gp);
 	});
 	return geoPlaces;
     };
-    
+
     function getURLParameter(name) {
 	p = decodeURI(
-            (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
+	    (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
 	);
 	if (p === "null") {
 	    return null;
@@ -220,4 +249,3 @@ $(document).ready(function() {
 	setTimeout(function() {maybeCenterMapOnUrlParam(map)}, 1000);
     });
 });
-
